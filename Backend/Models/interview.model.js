28ebfd42d8244e42d5d0d25de15b34089861213d
Anandhu_P_A <anandhupa131@gmail.com>
2023-07-28 20:16:42 +0530

    added backened router for live messages

--- Backend/Models/interview.model.js
+++ Backend/Models/interview.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 const interviewSchema = mongoose.Schema({
   userId: {type:String,required:true},
+  noOfQuestions:{type:Number,default:5},
   interviewData: {
     type: [
       {
@@ -13,8 +14,12 @@ const interviewSchema = mongoose.Schema({
     required: true,
     validate: [arrayMinLength, '{PATH} cannot be empty']
   },
+  subject:{type:String}
 
 },{versionKey:false,timestamps:true});
+
+
+
 
 function arrayMinLength(arr) {
   return arr.length > 0;
